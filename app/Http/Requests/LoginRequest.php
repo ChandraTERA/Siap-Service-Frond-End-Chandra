9d2ae0194ebf2da0a1e441a44b7729c5a0c40b82
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-05-01 00:20:24 +0700

    LoginRequest.php
    LoginRequest.php

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.email' => 'Format email salah',
+            'required' => 'Password dan email tidak boleh kosong'
+        ];
+    }
+}
