b042bc989552521e631442f130c3346e1bce4567
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-05-02 11:06:47 +0700

    DispatchSendEmailResetPassword.php
    DispatchSendEmailResetPassword.php

---
+++ app/Jobs/DispatchSendEmailResetPassword.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class DispatchSendEmailResetPassword implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    protected $data;
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $data = $this->data;
+        try {
+            Mail::send("resetPassword.reset_password_email", ['otp_reset_password' => $data['otp_reset_password']], function ($message) use ($data) {
+                $message->to($data['email']);
+                $message->subject("Reset Password");
+            });
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
+}
