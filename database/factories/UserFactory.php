939c9701bda002783a48f4da1b05807d5ff626ce
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-05-06 21:19:39 +0700

    userFactory.php
    userFactory.php

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama_lengkap' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'tgl_lahir' => fake()->date(),
+            'no_tlp' => '08123456786',
+            'alamat' => fake()->address(),
+            'kota' => fake()->city(),
+            'is_verified' => true,
+            'otp_created_at' => now(),
+            'password' => Hash::make('rahasia'),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
