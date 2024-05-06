011da4db95b1e6b2d63e83b6986607121b621b16
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-05-06 21:29:13 +0700

    2023_10_12_123848_create_devices_table.php
    2023_10_12_123848_create_devices_table.php

---
+++ database/migrations/2023_10_12_123848_create_devices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('fillpath_icon')->nullable();
+            $table->string('nama_device')->nullable();
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
