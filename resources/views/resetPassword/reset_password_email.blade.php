cb17eef764e7b55a5bcfe7c05b74ccabd4ad8c71
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-05-12 22:55:11 +0700

    reset_password_email.blade.php
    reset_password_email.blade.php

---
+++ resources/views/resetPassword/reset_password_email.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Reset Password</title>
+</head>
+
+<body>
+    <h1>Reset Password</h1>
+    <p>{{$otp_reset_password}}</p>
+    {{-- <a href="http://localhost:8000/api/reset_password/{{ $token }}">reset password</a> --}}
+</body>
+
+</html>
